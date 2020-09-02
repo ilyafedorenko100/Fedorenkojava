73c7dc26716b2589f3b3c6e1e528dd81244fae78
ilya.fedorenko <ilya.fedorenko@x5.ru>
2020-09-02 16:06:25 +0300

    тестовый ввод перем енных

---
+++ homework2/HomeWork2.java
@@ -0,0 +1,10 @@
+package homework2;
+public class HomeWork2 {
+        public static void main(String[] args) {
+            int a=199;
+            int b=111;
+            String c = "Start";
+            System.out.println(a+b+ " " +c+ "х5");
+        }
+    }
+
